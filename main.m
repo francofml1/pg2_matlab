79d058639bb10fe9aae4a6e30b138d3e3277ff65
Franco Louzada <franco.louzada@2solve.com>
2022-01-31 18:32:06 -0300

    Envia dados por MQTT

--- main.m
+++ main.m
@@ -1,16 +1,59 @@
-recObj = audiorecorder(44100,16,1)
+%%
+% Transmissão de sinal com modulação OOK
+%
+% Níveis [0,1]
+
+clear all, close all;  clc;
+myMQTT= mqtt('tcp://localhost');
+pub_topic = 'inTopic'
 
 %%
-disp('Start speaking.')
-recordblocking(recObj, 5);
-disp('End of Recording.');
+% config para graficos:
+fator = 170;
+img_w = 5 * fator;
+img_h = 3 * fator;
+line_w = 1.5;
 
-%%
-play(recObj);
+%
+%% ##############  Parametros de Entrada  ##############
+M = 2;          % Nível da modulação
+k = log2(M);    % bits por símbolo (=1 para M = 2)
+n = 100;   % Numero de bits da Sequencia (Bitstream)
 
-%%
-close all
-y = getaudiodata(recObj);
-t = linspace(0, 5, length(y));
-plot(t,y);
-grid on;
+nsamp = 4;      % Taxa de Oversampling
+Ts = 100e-3;    % Período de amostragem
+Tb = Ts;        % Tempo de bit
+Fs = 1 / Ts;    % Taxa de amostragem (amostras/s)
+snr_p = 15;     % SNR em dB
+
+
+% #### Parâmetros para ESP:
+delay_t = 50e-3;    % delay para desligar a saída após uma subida
+
+
+
+
+x = randi([0,M-1],n,1);     % gera sequência aleatória
+
+
+struct_data = struct('Tb', Tb, 'Td', delay_t, 'n', n, 'x_bit', x);
+json_data = jsonencode(struct_data);
+publish(myMQTT, pub_topic, json_data, 'Retain', false)
+
+%% Recpção por gravação de áudio:
+% recObj = audiorecorder(44100,16,1)
+
+% %%
+% disp('Start speaking.')
+% recordblocking(recObj, 5);
+% disp('End of Recording.');
+
+% %%
+% play(recObj);
+
+% %%
+% close all
+% y = getaudiodata(recObj);
+% t = linspace(0, 5, length(y));
+% plot(t,y);
+% grid on;
