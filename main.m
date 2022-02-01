7a93573ea296b8fcf545c1188ce1e8ffe2c13647
Franco Louzada <franco.louzada@2solve.com>
2022-02-01 18:05:42 -0300

    Alguns gráficos dos sinais no tempo

--- main.m
+++ main.m
@@ -7,7 +7,8 @@
 clear all, close all;  clc;
 
 myMQTT = mqtt('tcp://localhost');       % objeto MQTT
-data_topic = 'd';                       % tópido para publicacao de dados
+data_topic = 'd';                       % tópico para publicacao de dados
+Fs_audio = 44100;                       % taxa de amostragem do audio
 recObj = audiorecorder(44100, 16, 1, 1) % objeto para gravacao de audio
 
 % config para graficos:
@@ -20,16 +21,21 @@ line_w = 1.5;
 %% ##############  Parametros de Entrada  ##############
 M = 2;          % Nível da modulação
 k = log2(M);    % bits por símbolo (=1 para M = 2)
-n = 10;         % Numero de bits da Sequencia (Bitstream)
+n_prefix = 15;  % Comprimento do prefixo
+n_msg = 100;    % Comprimento da mensagem
+n = n_msg + n_prefix;         % Numero de bits da Sequencia (Bitstream)
 
-nsamp = 4;      % Taxa de Oversampling
 Tb = 100e-3;    % Tempo de bit
-Ts = Tb/nsamp;  % Período de amostragem
+Ts = Tb;        % Período de amostragem
 Fs = 1 / Ts;    % Taxa de amostragem (amostras/s)
-snr_p = 15;     % SNR em dB
+nsamp = Fs_audio / Fs;      % Taxa de Oversampling
 Tt = n * Tb;    % Tempo total do sinal
 
-% #### Parâmetros para ESP:
+% Vetores de tempo
+t_x = linspace(0, Tt, n);
+t_xup = linspace(0, Tt, n * nsamp);
+
+% Parâmetros para ESP:
 if (Tb > 30e-3)
     delay_t = Tb/2;    % delay para desligar a saída após uma subida
 else
@@ -37,10 +43,15 @@ else
 end
 
 % Sinal a ser transmitido:
-% x = ones(1, n);             % gera sequência de pulsos
-x = randi([0,M-1],n,1);     % gera sequência aleatória
+prefix = [ones(1, 10) zeros(1,5)];
+% x = [prefix ones(1, n - n_prefix)];             % gera sequência de pulsos
+x_rand = randi([0,M-1],n - n_prefix,1);     % gera sequência aleatória
+x = [prefix x_rand'];
 
-% Converte para json e transmite à ESP:
+% Reamostragem (upsample)
+x_up = rectpulse(x, nsamp);
+
+%% Converte para json e transmite à ESP:
 struct_data = struct('Tb', Tb, 'Td', delay_t, 'n', n, 'x_bit', x);
 json_data = jsonencode(struct_data);
 publish(myMQTT, data_topic, json_data, 'Retain', false)
@@ -51,10 +62,45 @@ disp('Iniciando gravação')
 recordblocking(recObj, Tt + 0.2);
 disp('Fim da gravação');
 
-%%
-close all
+%% Demodulação:
 y = getaudiodata(recObj);
-t = linspace(0, Tt * 1.1, length(y));
+y_demod = pamdemod(y, M);
+
+%% Gráficos
+t_y = linspace(0, Tt * 1.1, length(y));
 figure()
-plot(t,y);
+plot(t_y,y);
 grid on;
+
+figure('Name', 'Sinais no Tempo - upsample', 'Position', [50 50 img_w img_h])
+subplot(211)
+    p1=plot(t_x, x, '.-b');
+    title('Sinal Original')
+    ylabel('Amplitude')
+    xlabel('Tempo [ms]')
+    ylim([-0.2 1.2])
+    xlim([0 Tt*1.1])
+    grid on; hold on;
+    p1.LineWidth = 1.5;
+    
+subplot(212)
+% p2=plot(t(1:50), y(1:50), 'r');
+    p2=plot(t_xup, x_up, '.-b');
+    % p2=plot(t_y, y_demod, 'r');
+    title('Sinal Recebido')
+    ylabel('Amplitude')
+    xlabel('Tempo [ms]')
+    ylim([-0.2 1.2])
+    xlim([0 Tt*1.1])
+    grid on;
+    p2.LineWidth = 1.5;
+
+figure('Name', 'Sinais no Tempo - upsample', 'Position', [50 50 img_w img_h])
+    p3=plot(t_y, abs(y), 'b');
+    title('Sinal Recebido')
+    ylabel('Amplitude')
+    xlabel('Tempo [ms]')
+    % ylim([-0.2 1.2])
+    xlim([0 Tt*1.1])
+    grid on;
+    % p3.LineWidth = 1.5;
