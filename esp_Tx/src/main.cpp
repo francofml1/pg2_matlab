8152bf074b820083b753723e4b01edae7293147a
Franco Louzada <franco.louzada@2solve.com>
2022-02-01 15:15:51 -0300

    Recebe mensagens com buffer de até 8192 B

--- esp_Tx/src/main.cpp
+++ esp_Tx/src/main.cpp
@@ -4,19 +4,21 @@
 #include <ArduinoJson.h>
 
 #define D1 5
-#define MSG_BUFFER_SIZE (500)
+#define MSG_BUFFER_SIZE (8192)
+// #define MQTT_MAX_PACKET_SIZE (2*8192)
 
-const char *ssid = "Testando";
-const char *password = "76543210";
-const char *mqtt_server = "192.168.137.55";
-const int mqtt_port = 1883;
+const char *ssid = "SIMONI";
+const char *password = "simoni#S***";
+const char *brokerMQTT = "192.168.103.100";
+const int portMQTT = 1883;
 
 WiFiClient espClient;
 PubSubClient MQTT(espClient);
 unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 
-DynamicJsonDocument doc(4096);
+// DynamicJsonDocument doc(MSG_BUFFER_SIZE);
+StaticJsonDocument<MSG_BUFFER_SIZE> doc;
 
 // Protótipos de função:
 void setup_wifi();
@@ -33,7 +35,9 @@ void setup()
 
     Serial.begin(115200);
     setup_wifi();
-    MQTT.setServer(mqtt_server, mqtt_port);
+
+    MQTT.setBufferSize(MSG_BUFFER_SIZE);
+    MQTT.setServer(brokerMQTT, portMQTT);
     MQTT.setCallback(callback);
 }
 
@@ -48,7 +52,6 @@ void loop()
 
 void setup_wifi()
 {
-
     delay(10);
     // We start by connecting to a WiFi network
     Serial.println();
@@ -77,7 +80,7 @@ void callback(char *topic, byte *payload, unsigned int length)
     String msg;
 
     // obtem a string do payload recebido
-    for (int i = 0; i < length; i++)
+    for (uint32_t i = 0; i < length; i++)
     {
         char c = (char)payload[i];
         msg += c;
@@ -85,54 +88,39 @@ void callback(char *topic, byte *payload, unsigned int length)
     Serial.print("MSG recebida: ");
     Serial.println(msg);
 
-    // StaticJsonDocument<10000> doc;
+    // deserializeJson(doc, msg);
+    // JsonObject obj = doc.as<JsonObject>();
+    Serial.println("db 1");
+    DeserializationError error = deserializeJson(doc, msg);
+    Serial.println("db 2");
+    if (error)
+    {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        MQTT.publish("ERR", String(error.f_str()).c_str());
+        return;
+    }
+    Serial.println("db 3");
 
-    deserializeJson(doc, msg);
-    JsonObject obj = doc.as<JsonObject>();
-
-    double Tb = obj["Tb"];
+    double Tb = doc["Tb"];
+    double Td = doc["Td"];
+    unsigned int n = doc["n"];
     Tb *= 1000;
-    double Td = obj["Td"];
     Td *= 1000;
-    uint16_t n = obj["n"];
     bool x_bit[n];
+    Serial.println("db 4");
     for (size_t j = 0; j < n; j++)
     {
         x_bit[j] = doc["x_bit"][j].as<bool>();
     }
+    Serial.println("db 5");
 
-    Serial.println(sizeof(x_bit));
+    // x_bit = doc["x_bit"].as<bool *>();
+    // JsonArray x_bit = doc.createNestedArray("x_bit");
+
+    // Serial.println(sizeof(x_bit));
 
     sendArray(n, Tb, Td, x_bit);
-}
-
-void reconnect()
-{
-    // Loop until we're reconnected
-    while (!MQTT.connected())
-    {
-        Serial.print("Attempting MQTT connection...");
-        // Create a random client ID
-        String clientId = "ESP8266Client-";
-        clientId += String(random(0xffff), HEX);
-        // Attempt to connect
-        if (MQTT.connect(clientId.c_str()))
-        {
-            Serial.println("connected");
-            // Once connected, publish an announcement...
-            // MQTT.publish("outTopic", "hello world");
-            // ... and resubscribe
-            MQTT.subscribe("inTopic");
-        }
-        else
-        {
-            Serial.print("failed, rc=");
-            Serial.print(MQTT.state());
-            Serial.println(" try again in 5 seconds");
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
-    }
 }
 
 /**
@@ -147,8 +135,14 @@ void sendArray(uint16_t N, double Tb, double T_delay, bool *bit_array)
     for (size_t i = 0; i < N; i++)
     {
         T0_bit = millis();
+        Serial.print("x_bit[");
+        Serial.print(i);
+        Serial.print("]");
+        Serial.println(bit_array[i]);
+
         if (bit_array[i])
         {
+
             digitalWrite(D1, 1);
             digitalWrite(LED_BUILTIN, 1);
             delay(T_delay);
@@ -158,6 +152,39 @@ void sendArray(uint16_t N, double Tb, double T_delay, bool *bit_array)
         while ((millis() - T0_bit) < (Tb))
         {
             delay(5);
+        }
+    }
+    digitalWrite(LED_BUILTIN, 0);
+}
+
+void reconnect()
+{
+    digitalWrite(LED_BUILTIN, 1);
+
+    // Loop until we're reconnected
+    while (!MQTT.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        // Create a random client ID
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        // Attempt to connect
+        if (MQTT.connect(clientId.c_str()))
+        {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            // MQTT.publish("outTopic", "hello world");
+            // ... and resubscribe
+            MQTT.subscribe("d");
+            digitalWrite(LED_BUILTIN, 0);
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(MQTT.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
         }
     }
 }
