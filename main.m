41f0a79853ca980368e5ffaf5741521e191c8779
Franco Louzada <franco.louzada@2solve.com>
2022-02-01 16:43:19 -0300

    Recepção com gravação de áudio

--- main.m
+++ main.m
@@ -3,11 +3,13 @@
 %
 % Níveis [0,1]
 
+%% ############## Inicialização ##############
 clear all, close all;  clc;
-myMQTT= mqtt('tcp://localhost');
-data_topic = 'd';
 
-%%
+myMQTT = mqtt('tcp://localhost');       % objeto MQTT
+data_topic = 'd';                       % tópido para publicacao de dados
+recObj = audiorecorder(44100, 16, 1, 1) % objeto para gravacao de audio
+
 % config para graficos:
 fator = 170;
 img_w = 5 * fator;
@@ -18,14 +20,14 @@ line_w = 1.5;
 %% ##############  Parametros de Entrada  ##############
 M = 2;          % Nível da modulação
 k = log2(M);    % bits por símbolo (=1 para M = 2)
-n = 50;   % Numero de bits da Sequencia (Bitstream)
+n = 10;         % Numero de bits da Sequencia (Bitstream)
 
 nsamp = 4;      % Taxa de Oversampling
-Ts = 100e-3;    % Período de amostragem
-Tb = Ts;        % Tempo de bit
+Tb = 100e-3;    % Tempo de bit
+Ts = Tb/nsamp;  % Período de amostragem
 Fs = 1 / Ts;    % Taxa de amostragem (amostras/s)
 snr_p = 15;     % SNR em dB
-
+Tt = n * Tb;    % Tempo total do sinal
 
 % #### Parâmetros para ESP:
 if (Tb > 30e-3)
@@ -34,29 +36,25 @@ else
     delay_t = 15e-3;    % delay para desligar a saída após uma subida
 end
 
+% Sinal a ser transmitido:
+% x = ones(1, n);             % gera sequência de pulsos
+x = randi([0,M-1],n,1);     % gera sequência aleatória
 
-% x = randi([0,M-1],n,1);     % gera sequência aleatória
-x = ones(1, n);
-
+% Converte para json e transmite à ESP:
 struct_data = struct('Tb', Tb, 'Td', delay_t, 'n', n, 'x_bit', x);
 json_data = jsonencode(struct_data);
-
 publish(myMQTT, data_topic, json_data, 'Retain', false)
 
+
 %% Recpção por gravação de áudio:
-% recObj = audiorecorder(44100,16,1)
+disp('Iniciando gravação')
+recordblocking(recObj, Tt + 0.2);
+disp('Fim da gravação');
 
-% %%
-% disp('Start speaking.')
-% recordblocking(recObj, 5);
-% disp('End of Recording.');
-
-% %%
-% play(recObj);
-
-% %%
-% close all
-% y = getaudiodata(recObj);
-% t = linspace(0, 5, length(y));
-% plot(t,y);
-% grid on;
+%%
+close all
+y = getaudiodata(recObj);
+t = linspace(0, Tt * 1.1, length(y));
+figure()
+plot(t,y);
+grid on;
